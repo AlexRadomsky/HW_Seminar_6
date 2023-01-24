04734827390b80d3819e5968f6f9862799e0e44a
Alex Radomsky <radomsky2910@gmail.com>
2023-01-24 11:44:42 +0300

    Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4*/
+
+Console.Write("Enter the elements separated by a space: ");
+int[] numbers = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+int count = numbers.Count(x => x > 0);
+Console.Write($"Number of elements > 0: {count}");
